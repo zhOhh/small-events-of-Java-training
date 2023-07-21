85a07e51cf6bf2653ce4be4c446d693a955e517f
zhOhh <1299410261@qq.com>
2023-07-21 11:28:16 +0800

    V1.0

---
+++ TankGame/src/Hero.java
@@ -0,0 +1,7 @@
+package TankGame1;
+
+public class Hero extends Tank {
+    public Hero(int x, int y) {
+        super(x, y);
+    }
+}
---
+++ TankGame/src/MyPanel.java
@@ -0,0 +1,45 @@
+package TankGame1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyPanel extends JPanel {
+    Hero hero = null;
+
+    public MyPanel() {
+        hero = new Hero(100, 100);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.fillRect(0, 0, 1000, 750);
+
+        drawTank(hero.getX(), hero.getY(), g,0,0);
+    }
+
+
+    public void drawTank(int x, int y, Graphics g, int direct, int type) {
+        switch (type) {
+            case 0:
+                g.setColor(Color.cyan);
+                break;
+            case 1:
+                g.setColor(Color.yellow);
+        }
+
+        switch (direct) {
+            case 0:
+                g.fill3DRect(x,y,10,60,false);
+                g.fill3DRect(x+30,y,10,60,false);
+                g.fill3DRect(x+10,y+10,20,40,false);
+                g.fillOval(x+10,x+20,20,20);
+                g.drawLine(x+20,y+30,x+20,y);
+                break;
+            default:
+                System.out.println("无。。。。。");
+        }
+
+    }
+
+}
